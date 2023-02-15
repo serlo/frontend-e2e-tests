24581c073520f53a5f5e80c536a1c1201584b857
Hugo Tiburtino <hugotiburtino@protonmail.com>
2023-02-15 09:54:45 +0100

    refactor(ts): declare type for global login

--- tests/200-login.ts
+++ tests/200-login.ts
@@ -1,3 +1,5 @@
+import { faker } from '@faker-js/faker'
+
 Feature('Login')
 
 Scenario('Login user', ({ I }) => {
@@ -17,5 +19,24 @@ Scenario('Logout user', ({ I }) => {
   I.waitForText('Bis bald!', 10)
   I.see('Anmelden')
 })
-
 // make sure user is logged out at the end of the tests
+
+Feature('Registration')
+
+Scenario('Register user', ({ I }) => {
+  const verificationLink = '#mailDetails > p:nth-child(5) > a:nth-child(2)'
+
+  I.amOnPage('/')
+  I.click('Anmelden')
+  I.click('Einen neuen Account anlegen')
+  I.fillField('E-Mail-Adresse', faker.internet.email())
+  I.fillField('Benutzername', faker.internet.userName())
+  I.fillField('Passwort', '123456')
+  I.click('Account anlegen', '.serlo-button-green')
+  if (process.env.FRONTEND_API === 'local') {
+    I.amOnPage('http://localhost:4436')
+    I.click('Bitte bestätige deine E-Mail-Adresse')
+    I.click(verificationLink)
+    I.see('Mit deinem Account anmelden')
+  }
+})

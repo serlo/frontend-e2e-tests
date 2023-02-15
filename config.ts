d64e7a04d4732a6a9cda907df3ce89ffe12736c5
Hugo Tiburtino <hugotiburtino@protonmail.com>
2023-02-15 22:11:41 +0100

    refactor(config): declare if running locally

--- config.ts
+++ config.ts
@@ -3,8 +3,11 @@ import dotenv from 'dotenv'
 function createConfig() {
   dotenv.config()
 
+  const usesLocalApi = process.env.FRONTEND_API == 'local'
+
   return {
-    adminUser: process.env.FRONTEND_API == 'local' ? 'admin' : 'Kulla',
+    shouldRunOnlyLocally: usesLocalApi,
+    adminUser: usesLocalApi ? 'admin' : 'Kulla',
     isCI: Boolean(process.env.CI),
     frontendUrl: process.env.FRONTEND_URL ?? 'https://de.serlo-staging.dev',
   }

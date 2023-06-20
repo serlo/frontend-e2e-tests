3f76527579e5445478464cc1277872ff81c13387
Yannick Stritzke <yannick-stritzke@outlook.de>
2023-06-20 12:39:54 +0200

    Add Save changes
    add headline
    add some text and check
    click on save an check

--- tests/400-serlo-editor.ts
+++ tests/400-serlo-editor.ts
@@ -541,3 +541,30 @@ Scenario('Toolbar Toggle on and off', async ({ I }) => {
 
   I.dontSeeElement('span.katex')
 })
+
+Scenario('Save changes', async ({ I }) => {
+  I.amOnPage('/entity/create/Article/1377')
+
+  I.click('h1')
+
+  I.type('Test save changes')
+
+  I.click('Füge ein Element hinzu')
+
+  I.pressKey('Backspace')
+
+  I.type('Some text')
+
+  I.see('Some text')
+
+  I.click('button.serlo-button-green.ml-2')
+
+  I.see('Beschreibe deine Änderungen am Inhalt')
+
+  // Use class instead of text because text is super long and not reliable
+  I.click('.license-wrapper')
+
+  I.fillField('label textarea', 'I wrote some Text')
+  I.click('button.serlo-button.ml-2.serlo-button-green')
+  I.dontSee('Bitte alle Pflichtfelder ausfüllen')
+})

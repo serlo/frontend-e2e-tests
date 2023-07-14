af48240622224f49e74d0b8df757dd0b063064bc
Mikey Stengel <mikey@serlo.org>
2023-07-14 20:46:01 +0200

    Get rid of warning by overwriting onbeforeunload

--- tests/400-serlo-editor.ts
+++ tests/400-serlo-editor.ts
@@ -1,5 +1,17 @@
 Feature('Serlo Editor')
 
+const stopPopupPropagation = (event) => {
+  event.stopImmediatePropagation()
+}
+
+// Fixes warning "Popup already exists and was not closed. Popups must always be
+// closed by calling either I.acceptPopup() or I.cancelPopup()"
+Before(async ({ I }) => {
+  I.executeScript(() => {
+    window.onbeforeunload = null
+  })
+})
+
 Scenario('Basic text interactions', async ({ I }) => {
   I.amOnPage('/entity/repository/add-revision/74888')
   I.click('span[data-slate-node="text"]')
@@ -485,7 +497,7 @@ Scenario('Toolbar Toggle on and off', async ({ I }) => {
   I.click('Some text')
 
   I.click(
-    'button.serlo-button-editor-secondary.serlo-tooltip-trigger.ml-2.h-10.w-10',
+    'button.serlo-button-editor-secondary.serlo-tooltip-trigger.ml-2.h-10.w-10'
   )
 
   I.dontSeeElement({ css: '.serlo-editor-hacks a' })

eac3d44246be52fb43d4429914d7110909bedf8d
Vitomir Budimir <budimirvitomir@gmail.com>
2023-08-03 14:23:08 +0200

    test(editor): multimedia plugin settings

--- tests/400-serlo-editor.ts
+++ tests/400-serlo-editor.ts
@@ -755,6 +755,57 @@ Scenario('Toolbar Toggle on and off', async ({ I }) => {
   I.dontSeeElement('span.katex')
 })
 
+Scenario.only('Multimedia plugin controls', async ({ I }) => {
+  I.amOnPage('/entity/create/Article/1377')
+
+  // Ensure that only the Multimedia plugin from article introduction is present
+  I.seeNumberOfElements('$plugin-multimedia-wrapper', 1)
+
+  // Add a new Multimedia plugin and ensure that it is present
+  I.click('$add-new-plugin-row-button')
+  I.type('Multimedia')
+  I.pressKey('Enter')
+  I.seeNumberOfElements('$plugin-multimedia-wrapper', 2)
+
+  // When adding a new Multimedia plugin, the caption of its Image plugin is focused.
+  // In order to focus the actual Multimedia plugin, click the parent indicator.
+  I.click('$plugin-multimedia-parent-button')
+
+  // Change the size of the multimedia content
+  I.click('$plugin-multimedia-settings-button')
+  I.seeAttributesOnElements('$plugin-multimedia-size-button-50', {
+    'aria-selected': 'true',
+  })
+  I.seeAttributesOnElements('$plugin-multimedia-size-button-25', {
+    'aria-selected': 'false',
+  })
+  I.click('$plugin-multimedia-size-button-25')
+  I.seeAttributesOnElements('$plugin-multimedia-size-button-50', {
+    'aria-selected': 'false',
+  })
+  I.seeAttributesOnElements('$plugin-multimedia-size-button-25', {
+    'aria-selected': 'true',
+  })
+  I.click('$modal-close-button')
+  I.seeElement('.media-wrapper.mobile\\:w-1\\/4')
+
+  // Change the type of the multimedia content to video
+  I.click('$plugin-multimedia-settings-button')
+  I.selectOption('$plugin-multimedia-type-select', 'Video')
+  I.click('$modal-close-button')
+  I.seeElement(
+    locate('$plugin-video-placeholder').inside('$plugin-multimedia-wrapper'),
+  )
+
+  // Change the type of the multimedia content to GeoGebra
+  I.click('$plugin-multimedia-settings-button')
+  I.selectOption('$plugin-multimedia-type-select', 'GeoGebra Applet')
+  I.click('$modal-close-button')
+  I.seeElement(
+    locate('$plugin-geogebra-placeholder').inside('$plugin-multimedia-wrapper'),
+  )
+})
+
 Scenario('Save changes', async ({ I }) => {
   I.amOnPage('/entity/create/Article/1377')
 

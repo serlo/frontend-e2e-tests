00fbf3078e2a97607ea6fdc6128cc669e1af6bbf
Vitomir Budimir <budimirvitomir@gmail.com>
2023-08-10 15:02:57 +0200

    feat(text-plugin-list): test ordered list shortcuts

--- tests/423-text-plugin-list.ts
+++ tests/423-text-plugin-list.ts
@@ -11,7 +11,7 @@ Scenario('Unordered list shortcuts (indentation missing)', async ({ I }) => {
   I.click('$add-new-plugin-row-button')
   I.pressKey('Backspace')
 
-  I.say('Create a list')
+  I.say('Create an unordered list')
   I.type('- Some text')
   I.see('Some text', 'ul')
 
@@ -26,7 +26,6 @@ Scenario('Unordered list shortcuts (indentation missing)', async ({ I }) => {
   I.seeElement({ css: 'ul > li > ul > li' })
 
   I.say('Unindent list item on Shift+Tab')
-  I.dontSee('ul > li > ul > li')
   I.pressKey(['Shift', 'Tab'])
   I.dontSeeElement({ css: 'ul > li > ul > li' })
 
@@ -50,4 +49,48 @@ Scenario('Unordered list shortcuts (indentation missing)', async ({ I }) => {
   })
 })
 
-Scenario.todo('Ordered list shortcuts')
+Scenario('Ordered list shortcuts', async ({ I }) => {
+  I.amOnPage('/entity/create/Article/1377')
+
+  I.say('Add a new text plugin and delete the backslash')
+  I.click('$add-new-plugin-row-button')
+  I.pressKey('Backspace')
+
+  I.say('Create an ordered list')
+  I.type('First list item')
+  I.click('$plugin-toolbar-button-nummerierte-liste')
+  I.see('First list item', 'ol')
+
+  I.say('Add new list item on Enter')
+  I.pressKey('Enter')
+  I.type('Second list item')
+  I.see('Second list item', 'ol')
+
+  I.say('Indent list item on Tab')
+  I.dontSee('ol > li > ul > li')
+  I.pressKey('Tab')
+  I.seeElement({ css: 'ol > li > ul > li' })
+
+  I.say('Unindent list item on Shift+Tab')
+  I.pressKey(['Shift', 'Tab'])
+  I.dontSeeElement({ css: 'ol > li > ul > li' })
+
+  I.say('Exit the list on double Enter')
+  I.pressKey('Enter')
+  I.pressKey('Enter')
+  I.type('Exit the list')
+  I.dontSee('Exit the list', 'ol')
+
+  I.say('Clear Text in list')
+  I.pressKey(['CommandOrControl', 'A'])
+  I.pressKey('Backspace')
+  I.seeElement({
+    css: '.serlo-editor-hacks div[data-slate-editor="true"] ol:not(.unstyled-list)',
+  })
+
+  I.say('Remove empty list item on backspace')
+  I.pressKey('Backspace')
+  I.dontSeeElement({
+    css: '.serlo-editor-hacks div[data-slate-editor="true"] ol:not(.unstyled-list)',
+  })
+})

9a341d1462613f580790c20a57bcd5d056121e56
Vitomir Budimir <budimirvitomir@gmail.com>
2023-08-10 15:56:26 +0200

    feat(plugin-toolbar): test duplicate and remove plugin buttons

--- tests/410-plugin-toolbar.ts
+++ tests/410-plugin-toolbar.ts
@@ -4,8 +4,44 @@ Feature('Serlo Editor - plugin toolbar')
 
 Before(popupWarningFix)
 
-Scenario.todo('Duplicate plugin')
+Scenario('Duplicate plugin', async ({ I }) => {
+  I.amOnPage('/entity/create/Article/1377')
+  const initialTextPluginsCount = await I.grabNumberOfVisibleElements(
+    '$plugin-text-editor',
+  )
 
-Scenario.todo('Delete plugin')
+  I.click('$add-new-plugin-row-button')
+  I.pressKey('Backspace')
+  I.seeNumberOfElements('$plugin-text-editor', initialTextPluginsCount + 1)
 
+  I.moveCursorTo(
+    locate('[data-radix-collection-item]').inside('.plugin-toolbar'),
+  )
+  I.click('$duplicate-plugin-button')
+
+  I.seeNumberOfElements('$plugin-text-editor', initialTextPluginsCount + 2)
+})
+
+Scenario('Delete plugin', async ({ I }) => {
+  I.amOnPage('/entity/create/Article/1377')
+  const initialTextPluginsCount = await I.grabNumberOfVisibleElements(
+    '$plugin-text-editor',
+  )
+
+  I.click('$add-new-plugin-row-button')
+  I.pressKey('Backspace')
+  I.seeNumberOfElements('$plugin-text-editor', initialTextPluginsCount + 1)
+
+  I.moveCursorTo(
+    locate('[data-radix-collection-item]').inside('.plugin-toolbar'),
+  )
+  I.click('$remove-plugin-button')
+
+  I.seeNumberOfElements('$plugin-text-editor', initialTextPluginsCount)
+})
+
+// The anchor link copy tool is currently not rendered in Codecept.
+// If this feature passes experimental stage, it's worth finding a way
+// to render it in Codecept. Otherwise, this todo can be removed.
+// (https://trello.com/c/W6ZXhNpv/95-anchor-sprungmarke)
 Scenario.todo('Copy link to plugin')

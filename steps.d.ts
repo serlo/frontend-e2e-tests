27821629d34a8b92c4054122cc8888d1726aa7f8
Vitomir Budimir <budimirvitomir@gmail.com>
2023-08-31 15:08:21 +0200

    chore: run npx codeceptjs def

--- steps.d.ts
+++ steps.d.ts
@@ -1,10 +1,8 @@
 /// <reference types='codeceptjs' />
 
+
 declare namespace CodeceptJS {
-  interface SupportObject {
-    I: I
-    current: any
-  }
+  interface SupportObject { I: I, current: any, login: any }
   interface Methods extends Playwright {}
   interface I extends WithTranslation<Methods> {}
   namespace Translation {
